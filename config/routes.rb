da95a7212d08728d493f0769556f67ef6fa30823
Anas Hmamouch <anashmamouch90@gmail.com>
2015-06-12 16:08:21 +0100

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: {formats: 'json'} do 
+  	namespace :v1 do
+  		resources :logs
+    end
+  end
+  
   resources :logs
   root "logs#index"
 end
